4ba634cbfdd158f591b6159af33b1f6574765fef
VasylPopaz <pafos1997@gmail.com>
2024-06-05 16:24:12 +0200

    add AddCompanyButton, CompanyForm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import AddCompanyButton from './components/AddCompanyButton';
+
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
+      <AddCompanyButton />
     </main>
   );
 }
